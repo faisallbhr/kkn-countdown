f860f08cf0a30d5f20a526419a6bb4359f4f1ea2
Faisal Bahari <fbhr1112@gmail.com>
2024-05-29 16:43:43 +0700

    fix: date gmt+8

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,6 +3,8 @@ import CountdownComponent from "@/components/Countdown";
 export default function Home() {
   const startDate = new Date("2024-07-10");
   const endDate = new Date("2024-08-23");
+  const startDateGmtPlus8 = new Date(startDate.getTime() - 8 * 60 * 60 * 1000);
+  const endDateGmtPlus8 = new Date(endDate.getTime() - 8 * 60 * 60 * 1000);
 
   return (
     <main className="min-h-dvh flex flex-col justify-center items-center px-4">
@@ -12,7 +14,10 @@ export default function Home() {
       <h1 className="font-bold text-2xl md:text-4xl text-center">
         Universitas Jember
       </h1>
-      <CountdownComponent startDate={startDate} endDate={endDate} />
+      <CountdownComponent
+        startDate={startDateGmtPlus8}
+        endDate={endDateGmtPlus8}
+      />
     </main>
   );
 }
